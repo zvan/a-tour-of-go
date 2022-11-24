9f6fff0ab58d077e6f1f5255ebd66da1fb19b662
Matej Zvan <matej.zvan@3fs.si>
2022-11-24 14:58:29 +0100

    Exercise: Readers

---
+++ exercise-readers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"golang.org/x/tour/reader"
+)
+
+type MyReader struct{}
+
+
+func (MyReader) Read(b []byte) (n int, err error) {
+	for i := range b {
+		b[i] = 'A'
+	}
+	
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
